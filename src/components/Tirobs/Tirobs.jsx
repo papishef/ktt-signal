47bac4b9afe8e3c135f759d6832dcc4110cf5856
papishef <sheriff.malebo@gmail.com>
2023-03-16 18:49:08 +0100

    tirobs update

--- src/components/Tirobs/Tirobs.jsx
+++ src/components/Tirobs/Tirobs.jsx
@@ -9,9 +9,29 @@ const Tirobs = () => {
     return (
         <div>
             <MenuBarTirobs />
-            <div className="top-container">
+            <div className="top-container-tirobs">
                 <Slider slideImgs={tirobsSlideImgs} />
             </div>
+            <div id="product" className="product">
+                <h2>Our Product</h2>
+                <p>Tirob’s NDS is non-toxic and 300 times more effective than 
+                commercial bleach and does not release toxic fumes into the air. 
+                It kills off germs, eliminates odours and can be used safely in Homes, 
+                Schools, Hotels, Hospitals, Saloons, wounds dressing and skin conditions.</p>
+
+                <div className="product-pictures">
+                    
+                    <div className="product-picture-one">
+                    </div>
+                
+
+                    <div className="product-picture-two">
+                    </div>
+          
+                </div>
+
+            </div>
+
         </div>
     )
 }
